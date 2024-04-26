9882b569de053c339a9677e272bfe4d90ad18dd3
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2024-04-26 19:46:03 +0200

    format on write'

--- lua/plugins/formatter.lua
+++ lua/plugins/formatter.lua
@@ -1,17 +1,26 @@
 local function config()
-    require("formatter").setup({
-        filetype = {
-            lua = {
-                require("formatter.filetypes.lua").stylua
-            },
-            go = {
-                require("formatter.filetypes.go").gofmt
-            }
-        }
-    })
+	local augroup = vim.api.nvim_create_augroup
+	local autocmd = vim.api.nvim_create_autocmd
+	augroup("__formatter__", { clear = true })
+	autocmd("BufWritePost", {
+		group = "__formatter__",
+		command = ":FormatWrite",
+	})
+
+	require("formatter").setup({
+		filetype = {
+			lua = {
+				require("formatter.filetypes.lua").stylua,
+			},
+			go = {
+				require("formatter.filetypes.go").gofmt,
+				require("formatter.filetypes.go").goimports,
+			},
+		},
+	})
 end
 
 return {
-    "mhartington/formatter.nvim",
-    config = config
+	"mhartington/formatter.nvim",
+	config = config,
 }
