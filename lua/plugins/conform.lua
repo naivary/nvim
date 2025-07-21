66631cda93c9232a0fbb422be5c4e8b17feccaf3
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-07-21 19:25:30 +0200

    formatting working

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,23 +1,26 @@
 local function config()
-	local conform = require("conform")
-            conform.setup({})
-	-- conform.setup({
-	-- 	formatters_by_ft = {
-	-- 		go = { "goimports-reviser", "gofumpt" },
-	-- 		--lua = { "stylua" },
-	-- 	},
-	-- 	format_on_save = {
-	-- 		lsp_format = "fallback",
-	-- 		timeout_ms = 500,
-	-- 		async = false,
-	-- 	},
-	-- })
-	vim.keymap.set("n", "<leader>jf", function()
-	 	conform.format({})
-	 end, { desc = "[F]ormat" })
+    local conform = require("conform")
+    conform.setup({
+        formatters_by_ft = {
+            go = { "goimports-reviser", "gofumpt" },
+            lua = { "stylua" },
+        },
+        format_on_save = {
+            lsp_format = "fallback",
+            timeout_ms = 500,
+            async = false,
+        },
+    })
+    vim.keymap.set("n", "<leader>jf", function()
+        conform.format({
+            lsp_format = "fallback",
+            timeout_ms = 500,
+            async = false,
+        })
+    end, { desc = "[F]ormat" })
 end
 
 return {
-	"stevearc/conform.nvim",
-	config = config,
+    "stevearc/conform.nvim",
+    config = config,
 }
