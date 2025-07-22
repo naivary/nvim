cabb91266ad65c43c2e4fbbff352b0f22c8b5fb0
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-07-22 09:25:41 +0200

    adding noice

---
+++ lua/naivary/linter.lua
@@ -0,0 +1,12 @@
+local golangcilint = require("lint").linters.golangcilint
+
+golangcilint.args = {
+	"run",
+	"--output.json.path=stdout",
+	"--show-stats=false",
+	"--output.text.print-issued-lines=false",
+	"--output.text.print-linter-name=false",
+	function()
+		return vim.fn.fnamemodify(vim.api.nvim_buf_get_name(0), ":h")
+	end,
+}
--- lua/plugins/nvim-lint.lua
+++ lua/plugins/nvim-lint.lua
@@ -1,23 +1,25 @@
 local function config()
-    local lint = require("lint")
-    lint.linters_by_ft = {
-        go = { "golangcilint" }
-    }
+	local lint = require("lint")
+	pcall(require, "naivary.linter")
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
-        group = lint_augroup,
-        callback = function()
-            lint.try_lint()
-        end,
-    })
+	lint.linters_by_ft = {
+		go = { "golangcilint" },
+	}
 
-    vim.keymap.set("n", "<leader>ll", function()
-        lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
+	local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+	vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost" }, {
+		group = lint_augroup,
+		callback = function()
+			lint.try_lint()
+		end,
+	})
+
+	vim.keymap.set("n", "<leader>lf", function()
+		lint.try_lint()
+	end, { desc = "Trigger linting for current file" })
 end
 
 return {
-    "mfussenegger/nvim-lint",
-    config = config,
+	"mfussenegger/nvim-lint",
+	config = config,
 }
