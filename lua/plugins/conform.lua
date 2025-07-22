1a85f2ff996e5d5f81f5e1a5d7494a0f2601f77e
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-07-22 09:34:37 +0200

    added noice

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,36 +1,32 @@
 local function config()
-    local conform = require("conform")
-    conform.setup({
-        formatters = {
-            ['goimports-reviser'] = {
-                args = {
-                    '-rm-unused',
-                    '-set-alias',
-                    '-format',
-                    '$FILENAME'
-                }
-            }
-        },
-        formatters_by_ft = {
-            go = { "goimports-reviser", "gofumpt" },
-            lua = { "stylua" },
-        },
-        format_on_save = {
-            lsp_format = "fallback",
-            timeout_ms = 500,
-            async = false,
-        },
-    })
-    vim.keymap.set("n", "<leader>jf", function()
-        conform.format({
-            lsp_format = "fallback",
-            timeout_ms = 500,
-            async = false,
-        })
-    end, { desc = "[F]ormat" })
+	local conform = require("conform")
+	conform.setup({
+		formatters = {
+			["goimports-reviser"] = {
+				args = {
+					"-rm-unused",
+					"-set-alias",
+					"-format",
+					"$FILENAME",
+				},
+			},
+		},
+		formatters_by_ft = {
+			go = { "goimports-reviser", "gofumpt" },
+			lua = { "stylua" },
+		},
+	})
+
+	vim.keymap.set("n", "<leader>jf", function()
+		conform.format({
+			lsp_format = "fallback",
+			timeout_ms = 500,
+			async = false,
+		})
+	end, { desc = "[F]ormat" })
 end
 
 return {
-    "stevearc/conform.nvim",
-    config = config,
+	"stevearc/conform.nvim",
+	config = config,
 }
