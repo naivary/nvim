945b607751066cc708fd04090198ce18d970bf27
Sayed Mustafa Hussaini <SayedMustafaHussaini@outlook.de>
2025-09-13 15:35:40 +0200

    fucking syntax problem

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -7,11 +7,13 @@ local function config()
             markdown = { "deno_fmt" },
             json = { "jq" },
             yaml = { "yamlfmt" },
+        }
     })
+
     vim.keymap.set("n", "<leader>jf", function()
         conform.format({
             lsp_format = "fallback",
-            timeout_ms = 2000,
+            timeout_ms = 1000,
             async = false,
         })
     end, { desc = "[F]ormat" })
